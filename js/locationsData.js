d2777414eec2cb5c157c1fb988375f19daa945b8
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-12 13:37:14 -0500

    object outline for firstAndPike store

---
+++ js/locationsData.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('locations.js loaded');
+
+var locations = [
+  '1st and Pike'
+  ,'SeaTac Airport'
+  ,'Seattle Center'
+  ,'Capitol Hill'
+  ,'Alki'
+];
+
+var firstAndPike = {
+  location: '1st and Pike'
+  ,address: ''
+  ,hoursOfOperation: ''
+  ,minHourlyCustomers: ''
+  ,maxHourlyCustomers: ''
+  ,avgCookiesPerSale: ''
+  ,randCustomersPerHour: function(){
+    //do a thing here
+  }
+};
