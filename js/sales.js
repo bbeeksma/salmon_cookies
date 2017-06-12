6b80b7fcb42a4e4dcb0f2d6fe49b4f60a04e0466
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-12 17:29:45 -0500

    array of locations working

--- js/sales.js
+++ js/sales.js
@@ -2,4 +2,10 @@
 
 console.log('sales.js loaded');
 
-for(var i = 0; i < locations.length; i++)
+for(var i = 0; i < locations.length; i++){
+  var newHeader = document.createElement('h2');
+  var newText = document.createTextNode(locations[i].location);
+  newHeader.appendChild(newText);
+  var position = document.getElementsByTagName('body')[0];
+  position.appendChild(newHeader);
+}
